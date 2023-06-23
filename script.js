65d8ab12bebb8dbabcae109761ebf28774d634ef
Stephan Frolov <stefro97@yandex.ru>
2023-06-23 12:54:29 +0300

    final commit

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+// Time Coundown
+let time = 10; // time in minutes
+let promoTime = time * 60;
 
+let counting = document.getElementById("countdown");
+
+function startCountdown() {
+  let promoTimer = setInterval(() => {
+    if (promoTime <= 0) {
+      clearInterval(promoTimer);
+      counting.innerHTML = "Promo has ended";
+    } else {
+      promoTime--;
+      const days = Math.floor(promoTime / 3600 / 24);
+      const hours = Math.floor(promoTime / 3600) % 24;
+      const min = Math.floor(promoTime / 60) % 60;
+      const sec = Math.floor(promoTime % 60);
+
+      counting.innerHTML = `TIME:  ${format(hours)}hr ${format(min)}m ${format(
+        sec
+      )}s`;
+    }
+  }, 1000);
+}
+
+function format(t) {
+  return t < 10 ? `0${t}` : t;
+}
+
+startCountdown();
